bd9a389e1ae8d2b5106da6b83029f77e6ea5c81a
pauloulisses <pauloulisses1212@gmail.com>
2026-04-13 15:15:22 -0300

    adiciona Brand no admin

--- cars/models.py
+++ cars/models.py
@@ -3,23 +3,20 @@ from django.db import models
 
 # tabela de marcas
 class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
 
 
 # tabela de carros
 class Car(models.Model):
+    id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.ForeignKey(
-        Brand,
-        on_delete=models.PROTECT,
-        related_name='car_brand'
-    )
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-
+    
     def __str__(self):
         return self.model
+       
+  
